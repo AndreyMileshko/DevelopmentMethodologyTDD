c72c71893b59413df0091865d3274f0d0688e135
Andrey Mileshko <AndreyPrograming.Mileshko@yandex.ru>
2024-09-03 15:11:17 +0700

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,3 +1,12 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+
+    public final Map<String, Long> phoneBookMap = new TreeMap<>();
+
+    public int add(String name, Long number) {
+        return 0;
+    }
 
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,26 @@
-public class PhoneBookTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PhoneBookTest {
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    void preparatoryProcedures() {
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    void afterAddingAnItemTheAddMethodWillReturn1() {
+        int size = phoneBook.add("TestName", 88005009120L);
+        assertEquals(1, size);
+    }
+
+    @Test
+    void checkThatTheAddMethodAddsValidElement() {
+        phoneBook.add("TestName", 88005009120L);
+        assertTrue(phoneBook.phoneBookMap.containsKey("TestName"));
+    }
 }
